5797968b87ed27dd45b98ed79368df3698eb6264
Daryna-Mariia <stankevychdaryna@gmail.com>
2023-03-27 18:12:11 +0300

    copy all from folder where i did homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+function makeGalleryMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href=${original}>
+        <img
+          class="gallery__image"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
+        />
+      </a>
+    </div>`;
+    })
+    .join('');
+}
+
+refs.gallery.insertAdjacentHTML('afterbegin', makeGalleryMarkup(galleryItems));
+refs.gallery.addEventListener('click', createGalleryModal);
+
+function createGalleryModal(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  window.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      modal.close();
+    }
+  });
+
+  const modal = basicLightbox.create(`<img src="${getImgURL(event)}">`);
+  modal.show();
+}
+
+function getImgURL(event) {
+  event.preventDefault();
+
+  return event.target.dataset.source;
+}
+
