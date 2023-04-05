51137c3875886d4d346ae5eca6f7c60cf1547bee
Daryna-Mariia <stankevychdaryna@gmail.com>
2023-04-05 13:52:59 +0300

    fix bag

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+  };
+  
+  function makeGalleryMarkup(arr) {
+    return arr
+      .map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            alt='${description}'
+          />
+        </a>`;
+      })
+      .join('');
+  }
+  
+  refs.gallery.insertAdjacentHTML('afterbegin', makeGalleryMarkup(galleryItems));
+  
+  let gallery = new SimpleLightbox('.gallery a');
+  
+  gallery.on('show.simplelightbox', function () {
+    gallery.options.captionDelay = 250;
+    gallery.options.captionsData = 'alt';
+  });
+  
